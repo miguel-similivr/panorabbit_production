7edcc69f444b251a6aa792155b49cf6bf7d799c0
miguel-similivr <miguel@similivr.com>
2016-09-07 00:15:12 -0700

    trying out lato font

--- nav/header.php
+++ nav/header.php
@@ -21,6 +21,9 @@
       
     <!-- Custom styles for this template -->
     <link href="/css/vr-template.css" rel="stylesheet">  
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
       
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
