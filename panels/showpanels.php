072a274cd64d7b5bfabe63b243f12975e3b737a7
miguel-similivr <miguel@similivr.com>
2016-09-12 00:59:33 -0700

    testing playlist

--- panels/showpanels.php
+++ panels/showpanels.php
@@ -105,5 +105,51 @@ function mostRecent($contentmysqli, $recentarray) {
 	}
 }
 
+function showPlaylist($contentmysqli, $playlistarray) {
+	global $playlistarray;
+	$plitems = array();
+
+	if ($select_stmt = $contentmysqli->prepare("SELECT content_id FROM panorabbit_playlist_items WHERE playlist_id = ?")) {
+	$select_stmt->bind_param('i', $_GET['playlist']);
+	// Execute the prepared query.
+	$select_stmt->execute();
+	$select_stmt->bind_result($plitemid);
+
+	while ($select_stmt->fetch()) {
+		array_push($plitems, $plitemid);
+		}
+	}
+
+	foreach ($plitems as $item) {
+		if ($select_stmt = $contentmysqli->prepare("SELECT username,thumbnail_url,views FROM panorabbit_contenturl WHERE id = ?")) {
+		$select_stmt->bind_param('i', $item);
+		// Execute the prepared query.
+		$select_stmt->execute();
+		$select_stmt->bind_result($displayuser, $displayurl, $displayviews);
+
+		while ($select_stmt->fetch()) {
+			$object = new contentobject;
+			$object->contentobjectuser = $displayuser;
+			$object->contentobjectid = $item;
+			$object->contentobjecturl = $displayurl;
+			$object->contentobjectviews = $displayviews;
+			array_push($playlistarray, $object);
+	   }
+		}
+
+		foreach ($playlistarray as $value) {
+			if ($meta_stmt = $contentmysqli->prepare("SELECT title,description FROM panorabbit_metadata WHERE content_id = ? LIMIT 1")) {
+				$meta_stmt->bind_param('i', $value->contentobjectid);
+				$meta_stmt->execute();
+				$meta_stmt->bind_result($displaytitle, $displaydescription);
+
+				while ($meta_stmt->fetch()) {
+					$value->contentobjecttitle = $displaytitle;
+					$value->contentobjectdescription = $displaydescription;
+				}
+			}
+		}
+	}
+}
 
 ?>
---
+++ playlist_test_f4nzt7tp.php
@@ -0,0 +1,75 @@
+<?php
+include_once '../includes/db_connect.php';
+include_once '../includes/functions.php';
+include_once '../includes/register.inc.php';
+include_once '../includes/contentdb_connect.php';
+include_once '../includes/content-config.php';
+
+$playlistarray = array();
+ 
+sec_session_start();
+?>
+
+<?php require("nav/include_nav.php"); ?>
+<?php insertTitle(["title" => "Panorabbit Test Playlist"]); ?>
+<?php include('panels/showpanels.php'); ?>
+<script src="/panels/createpanels.js"></script>
+
+<div class="container home-board" id="playlistcontainer"> 
+  <div class="col-lg-12 white-background">
+    <div class="col-lg-12 white-background">
+      <div class="col-lg-6 col-xs-6"><h4>Test Playlist</h4></div>
+      <div class="col-lg-6 col-xs-6 view-more"><a>view more</a></div>
+    </div>
+    <div class="white-background">
+        <?php showPlaylist($contentmysqli, $playlistarray); ?>
+        <script type="text/javascript">
+        var objects = <?php echo json_encode($playlistarray);?>;
+        for (var p in objects) {
+          createpanel(objects[p], p, "playlistcontainer");
+        }
+        </script>
+      </div>
+  </div>
+</div>
+
+
+        <script> $(document).ready( function() {
+
+        $('.thumbnail-item').hover( function() {
+            $(this).find('.thumbnail-detail').fadeIn(300);
+            }, 
+                                   function() {
+            $(this).find('.thumbnail-detail').fadeOut(100);
+        });
+    
+        $('#commentbody').click(function(){
+
+            $(".summit-comment").show();
+        } );
+    
+        $('.cancel-comment').click(function(){
+
+            $(".summit-comment").hide();
+        } );
+    
+    
+    
+       });
+      </script>  
+      
+
+
+</body>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <p>Copyright &copy; Simili Virtual Reality Inc. 2016</p>
+        <p><a href="../terms_of_service.html">Terms of Service</a></p>
+        <p><a href="../privacy_policy.html">Privacy Policy</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+</html>
