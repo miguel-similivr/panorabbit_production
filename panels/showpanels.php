3dabaabbfe085ab5d68cf1de067f591f718569f2
miguel-similivr <miguel@similivr.com>
2016-10-04 00:55:07 -0700

    finalling new query for playlist

--- panels/showpanels.php
+++ panels/showpanels.php
@@ -174,48 +174,26 @@ function showSearch($contentmysqli, $searcharray, $searchinput) {
 
 function showPlaylist($contentmysqli, $playlistarray) {
 	global $playlistarray;
-	$plitems = array();
-
-	if ($select_stmt = $contentmysqli->prepare("SELECT content_id FROM panorabbit_playlist_items WHERE playlist_id = ?")) {
+	if ($select_stmt = $contentmysqli->prepare("SELECT panorabbit_contenturl.id,panorabbit_contenturl.username,panorabbit_contenturl.thumbnail_url,panorabbit_contenturl.views,panorabbit_metadata.title,panorabbit_metadata.description
+		FROM `panorabbit_playlist_items`
+		LEFT JOIN `panorabbit_contenturl` on panorabbit_playlist_items.content_id = panorabbit_contenturl.id
+		LEFT JOIN `panorabbit_metadata` on panorabbit_playlist_items.content_id = panorabbit_metadata.content_id
+		WHERE panorabbit_playlist_items.playlist_id = ?")) {
 	$select_stmt->bind_param('i', $_GET['p']);
 	// Execute the prepared query.
 	$select_stmt->execute();
-	$select_stmt->bind_result($plitemid);
+	$select_stmt->bind_result($displayid, $displayuser, $displayurl, $displayviews, $displaytitle, $displaydescription);
 
 	while ($select_stmt->fetch()) {
-		array_push($plitems, $plitemid);
-		}
-	}
-
-	foreach ($plitems as $item) {
-		if ($select_stmt = $contentmysqli->prepare("SELECT username,thumbnail_url,views FROM panorabbit_contenturl WHERE id = ?")) {
-		$select_stmt->bind_param('i', $item);
-		// Execute the prepared query.
-		$select_stmt->execute();
-		$select_stmt->bind_result($displayuser, $displayurl, $displayviews);
-
-		while ($select_stmt->fetch()) {
-			$object = new contentobject;
-			$object->contentobjectuser = $displayuser;
-			$object->contentobjectid = $item;
-			$object->contentobjecturl = $displayurl;
-			$object->contentobjectviews = $displayviews;
-			array_push($playlistarray, $object);
-	   }
-		}
-
-		foreach ($playlistarray as $value) {
-			if ($meta_stmt = $contentmysqli->prepare("SELECT title,description FROM panorabbit_metadata WHERE content_id = ? LIMIT 1")) {
-				$meta_stmt->bind_param('i', $value->contentobjectid);
-				$meta_stmt->execute();
-				$meta_stmt->bind_result($displaytitle, $displaydescription);
-
-				while ($meta_stmt->fetch()) {
-					$value->contentobjecttitle = $displaytitle;
-					$value->contentobjectdescription = $displaydescription;
-				}
-			}
-		}
+		$object = new contentobject;
+		$object->contentobjectuser = $displayuser;
+		$object->contentobjectid = $displayid;
+		$object->contentobjecturl = $displayurl;
+		$object->contentobjectviews = $displayviews;
+		$object->contentobjecttitle = $displaytitle;
+		$object->contentobjectdescription = $displaydescription;
+		array_push($playlistarray, $object);
+   }
 	}
 }
 
