192cf318e61e2fbe6cc388106c46c71c70c39c58
miguel-similivr <miguel@similivr.com>
2016-10-09 01:31:08 -0700

    added pano url to ios/get_content.php

--- ios/get_content.php
+++ ios/get_content.php
@@ -1,17 +1,12 @@
 <?php
-
-include_once '../../includes/functions.php';
 include_once '../../includes/contentdb_connect.php';
 include_once '../../includes/content-config.php';
-
-sec_session_start();
-
-$error_msg = "";
 
 class contentobject {
 	public $contentobjectuser;
 	public $contentobjectid;
-	public $contentobjecturl;
+	public $contentobjectpano;
+	public $contentobjectthumb;
 	public $contentobjecttitle;
 	public $contentobjectdescription;
 	public $contentobjectviews;
@@ -19,18 +14,19 @@ class contentobject {
 
 $iosarray = array();
 
-if ($select_stmt = $contentmysqli->prepare("SELECT panorabbit_contenturl.id,panorabbit_contenturl.username,panorabbit_contenturl.thumbnail_url,panorabbit_contenturl.views,panorabbit_metadata.title,panorabbit_metadata.description 
+if ($select_stmt = $contentmysqli->prepare("SELECT panorabbit_contenturl.id,panorabbit_contenturl.username,panorabbit_contenturl.url,panorabbit_contenturl.thumbnail_url,panorabbit_contenturl.views,panorabbit_metadata.title,panorabbit_metadata.description 
 	FROM `panorabbit_contenturl`
 	LEFT JOIN `panorabbit_metadata` on panorabbit_contenturl.id = panorabbit_metadata.content_id")) {
 	// Execute the prepared query.
 	$select_stmt->execute();
-	$select_stmt->bind_result($displayid, $displayuser, $displayurl, $displayviews, $displaytitle, $displaydescription);
+	$select_stmt->bind_result($displayid, $displayuser, $displaypano, $displaythumb, $displayviews, $displaytitle, $displaydescription);
 
 	while ($select_stmt->fetch()) {
 		$object = new contentobject;
 		$object->contentobjectuser = $displayuser;
 		$object->contentobjectid = $displayid;
-		$object->contentobjecturl = $displayurl;
+		$object->contentobjectpano = $displaypano;
+		$object->contentobjectthumb = $displaythumb;
 		$object->contentobjectviews = $displayviews;
 		$object->contentobjecttitle = $displaytitle;
 		$object->contentobjectdescription = $displaydescription;
